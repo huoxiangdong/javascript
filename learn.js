66a0733b10369fa31a724c3a45d1c8b88201e802
huoxiangdong <huoxiangdong>
2017-02-08 10:48:25 +0800

    update learn.js

--- learn.js
+++ learn.js
@@ -1,3 +1,4 @@
+console.log('数组');
 /*var name = ['a','b','c'];
 var score = ['50','20','30'];*/
 /*name.length = 20;*/
@@ -9,6 +10,12 @@ console.log(name.length,score.length)*/
 /*console.log(name);*/
 /*console.log(score.indexOf('20'),score.length);*/
 
-var arr = [[1, 2, 3], [400, 500, 600], '-'];
+var arr = [[1, 2, 3], [400, 500, 600], '-']; //多维数组
 var x = arr[1].slice(1,2);
 console.log(x);
+/*var arr = ['小明', '小红', '大军', '阿黄'];
+arr.length = 30;
+alert(arr.length);
+alert('欢迎'+arr.slice(0,3)+'和'+arr[3]+'同学！');*/
+
+console.log('对象');
